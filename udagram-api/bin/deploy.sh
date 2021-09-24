66d52d9c558342547ecbfe5660bd67c86c544ec3
Todd <todd@useequipped.com>
2021-09-24 14:27:05 -0700

    add revised pipeline 10

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,1 +1,14 @@
+eb init
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
+            POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+            POSTGRES_DB=$POSTGRES_DB
+            POSTGRES_HOST=$POSTGRES_HOST
+            PORT=$PORT
+            AWS_REGION=$AWS_REGION
+            AWS_PROFILE=$AWS_PROFILE
+            AWS_BUCKET=$AWS_BUCKET
+            URL = $URL
+            AWS_ACCESS_KEY=$AWS_ACCESS_KEY
+            AWS_SECRET=$AWS_SECRET
+            JWT_SECRET=$JWT_SECRET
 eb deploy
